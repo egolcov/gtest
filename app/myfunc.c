3127b2696830330d52fa24754b061530a8856131
egolcov <egolcov030@gmail.com>
2024-03-08 20:06:50 +0300

    3commit

--- app/myfunc.c
+++ app/myfunc.c
@@ -4,10 +4,7 @@
 #include "myfunc.h"
 
 int myfunc(int b) {
-    char *buffer = malloc(sizeof(char) * 1000);
-    buffer [0] = b + 4;
-    // здесь должен ругаться sonarcloud, т.к. утечка памяти
-    return buffer[0];
+    return(b+4);
 }
 
 int val;
